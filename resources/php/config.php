789103cdf90bf92b59870f3ad2af974afddce7c4
David <iveyd@rpi.edu>
2016-12-08 11:19:19 -0500

    fixed spelling in file name

---
+++ resources/php/config.php
@@ -0,0 +1,10 @@
+<?php
+
+	$config = array(
+   	'DB_HOST'     => 'localhost',
+   	'DB_NAME'			=> 'collection_tracker',
+   	'DB_USERNAME' => 'root',
+  	'DB_PASSWORD' => 'test_password'
+	);
+
+?>
